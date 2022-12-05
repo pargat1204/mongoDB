1feca42b6acd95dc85deba7e5f8bc87c6770c72e
PARGAT SINGH <107990987+pargat1204@users.noreply.github.com>
2022-12-05 12:39:30 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,25 @@
+let {MongoClient} = require('mongodb');
+
+async function main(){
+    let client = new MongoClient("mongodb+srv://demo:nBB1BOLMXqMSExkG@cluster0.so1ntwj.mongodb.net/?retryWrites=true&w=majority");
+    try{
+        await client.connect();
+        await listDatabases(client);
+    }
+    catch(e){
+        console.log(e);
+    }
+    finally{
+        await client.close();
+    }   
+};
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    let databaseList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databaseList.databases.forEach(db => {
+        console.log(`- ${db.name}`);
+    });
+};
